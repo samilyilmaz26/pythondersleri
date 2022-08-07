ef6f6370b0d328e509fc556f134872daeef4a7e4
Samil <Samil@DESKTOP-PG28H37>
2022-08-07 21:53:05 +0300

    new folder

--- 12-VeriAnaliz/3-VeriGorsellestirme/2B-MelborneHouse/kirilimlar.py
+++ 12-VeriAnaliz/3-VeriGorsellestirme/2B-MelborneHouse/kirilimlar.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 
 df = pd.read_csv("melb_data.csv")
+
 print(df)
 print(df.info())
 print(df.dtypes)
@@ -10,12 +11,13 @@ print(df.isnull().values.sum())
 df.fillna(0,inplace=True)
 print(df.isnull().values.sum())
 print( sns.catplot(x= "Type",y= "Price",data=df))
-print (sns.barplot(x= "Type",y="Price",hue ="Regionname",data = df))
+print (sns.barplot(x= "Type",y="Price",
+hue ="Regionname",data = df))
 print (sns.barplot(x= "Type",y="Price",hue ="Method" ,data = df))
 print( sns.catplot(x= "Method",y= "Price",data=df))
 print( sns.catplot(x= "Method",y= "Price",hue="Regionname",data=df))
 print( sns.catplot(x= "Regionname",y= "Price",data=df))
 print( sns.catplot(x= "Regionname",y= "Price",hue="Type",data=df))
- 
+print("*******,**") 
  
 
